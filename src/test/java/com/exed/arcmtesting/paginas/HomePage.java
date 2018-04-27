bb7d3e392456d06eb3d30c1dc679dfeefb8d2cff
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-04-27 17:21:49 -0300

    Ajuste de navegação da pagina home para a explorer

--- src/test/java/com/exed/arcmtesting/paginas/HomePage.java
+++ src/test/java/com/exed/arcmtesting/paginas/HomePage.java
@@ -1,8 +1,11 @@
 package com.exed.arcmtesting.paginas;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	
@@ -12,12 +15,25 @@ public class HomePage {
 		this.driver = driver;
 	}
 	
-	public ExplorerPage acessaMenuExplorer(){
+	public ExplorerPage navegarParaExplorerPage(){
+		this.waitForElementPresent(By.name("head"), 10);
 		WebElement frameElem = driver.findElement(By.name("head"));
 		WebDriver frame = driver.switchTo().frame(frameElem);
 		String xPath = "/html/body/table/tbody/tr[1]/td/table/tbody/tr/td[2]/div[2]";
 		frame.findElement(By.xpath(xPath)).click();
+			
 		return new ExplorerPage(driver);
+	}
+	
+	public void waitForElementPresent(final By by, int timeout) {
+		WebDriverWait wait = (WebDriverWait) new WebDriverWait(driver, timeout)
+				.ignoring(StaleElementReferenceException.class);
+		wait.until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver webDriver) {
+				WebElement element = webDriver.findElement(by);
+				return element != null && element.isDisplayed();
+			}
+		});
 	}
 
 }
--- src/test/java/com/exed/arcmtesting/testes/auditoria/TestarFluxoDeApontamento.java
+++ src/test/java/com/exed/arcmtesting/testes/auditoria/TestarFluxoDeApontamento.java
@@ -3,8 +3,7 @@ package com.exed.arcmtesting.testes.auditoria;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.exed.arcmtesting.paginas.HomePage;
 import com.exed.arcmtesting.paginas.LoginPage;
@@ -16,10 +15,13 @@ public class TestarFluxoDeApontamento {
 	
 	@Before
 	public void inicializa(){
-		System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("no-sandbox");
-		driver = new ChromeDriver();	
+//		System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("no-sandbox");
+//		driver = new ChromeDriver();	
+//		login = new LoginPage(driver);
+		System.setProperty("webdriver.gecko.driver", "C:/GeckoDriver/geckodriver.exe");
+		driver = new FirefoxDriver();
 		login = new LoginPage(driver);
 	}
 	
@@ -28,7 +30,7 @@ public class TestarFluxoDeApontamento {
 		
 		login.visita();
 		HomePage home = login.executaLogin("caio.robert", "manager");
-		home.acessaMenuExplorer().acessaMenuApontamento();
+		home.navegarParaExplorerPage();
 		
 	}
 	
